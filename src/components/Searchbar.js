9f868521761e870a1e6592f59384dc484f7fddd0
Shaun Nelson <shaunptnelson@gmail.com>
2023-01-24 12:29:17 -0500

    1

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -30,10 +30,10 @@ const Searchbar = () => {
                 method: "POST",
                 headers: {
                   "Content-type": "application/json",
-                  "Access-Control-Allow-Origin": "*",
-                  "Access-Control-Allow-Credentials": "true",
-                  "Access-Control-Allow-Methods": "POST",
-                  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+                  // "Access-Control-Allow-Origin": "*",
+                  // "Access-Control-Allow-Credentials": "true",
+                  // "Access-Control-Allow-Methods": "POST",
+                  // "Access-Control-Allow-Headers": "Content-Type, Authorization",
                 },
                 body: payload,
               })
--- src/components/Track.js
+++ src/components/Track.js
@@ -15,10 +15,10 @@ const Track = (props) => {
       method: "POST",
       headers: {
         "Content-type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Methods": "POST",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        // "Access-Control-Allow-Origin": "*",
+        // "Access-Control-Allow-Credentials": "true",
+        // "Access-Control-Allow-Methods": "POST",
+        // "Access-Control-Allow-Headers": "Content-Type, Authorization",
       },
       body: JSON.stringify({ track: props.id }),
     })
